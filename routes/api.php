cd2d17bda8c05035e851d66a78f3611a9a2d004e
Anastasia-Mishalova <Nastya18.02@ya.ru>
2025-11-13 22:01:42 +0500

    Добавила роуты

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tasks', TaskController::class);
+Route::apiResource('tasks', TaskController::class) -> middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']) -> middleware('auth:sanctum');
