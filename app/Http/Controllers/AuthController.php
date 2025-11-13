d8c1a2cd0d32d160329a92bcaa1746915133c91d
Anastasia-Mishalova <Nastya18.02@ya.ru>
2025-11-13 20:53:02 +0500

    Написала функцию для регистрации

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+            'email' => 'required|email'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    public function login(Request $request) {}
+
+    public function logout(Request $request) {}
 }
