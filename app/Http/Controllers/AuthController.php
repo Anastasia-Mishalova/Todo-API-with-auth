df495449d42a867097031a31720633b7811f62fb
Anastasia-Mishalova <Nastya18.02@ya.ru>
2025-11-13 21:02:14 +0500

    Написала функцию login для входа в систему

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,12 +19,28 @@ class AuthController extends Controller
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password)
+            'password' => Hash::make($request->password)
         ]);
         return response()->json(['message' => 'User created successfully'], 201);
     }
 
-    public function login(Request $request) {}
+    public function login(Request $request)
+    {
+        $request->validate([
+            'password' => 'required|string|min:6',
+            'email' => 'required|email'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw new \Exception('Invalid credentials');
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
 
     public function logout(Request $request) {}
 }
