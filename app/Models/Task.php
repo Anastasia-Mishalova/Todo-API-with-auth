e90a2549cdd4c705e647cbadd699bee61e04d548
Anastasia-Mishalova <Nastya18.02@ya.ru>
2025-11-16 20:23:36 +0500

    Правки для вывода задач только конкретного авторизованного пользователя

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-     protected $fillable = [
+    protected $fillable = [
         'title',
         'description',
         'status',
+        'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
